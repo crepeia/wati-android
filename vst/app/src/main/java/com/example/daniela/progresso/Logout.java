08d51781ba05618c3b27cdf2ad0b940af9efe0fa
daanii <cwsdanipereira@gmail.com>
2017-04-24 15:37:24 -0300

    melhorias logout

--- vst/app/src/main/java/com/example/daniela/progresso/Logout.java
+++ vst/app/src/main/java/com/example/daniela/progresso/Logout.java
@@ -1,12 +1,9 @@
 package com.example.daniela.progresso;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
@@ -21,20 +18,45 @@ public class Logout extends AppCompatActivity {
         setContentView(R.layout.activity_logout);
 
 
-        if (AccessToken.getCurrentAccessToken() == null) {
-            return; // already logged out
-        }
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
 
-        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest.Callback() {
-            @Override
-            public void onCompleted(GraphResponse graphResponse) {
+        alertDialog.setTitle("Deseja realmente sair?");
 
-                LoginManager.getInstance().logOut();
+        alertDialog.setPositiveButton("Sim",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int which) {
+                        // Codigo para enviar
+                        if (AccessToken.getCurrentAccessToken() == null) {
+                            return; // already logged out
+                        }
 
-            }
-        }).executeAsync();
+                        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest.Callback() {
+                            @Override
+                            public void onCompleted(GraphResponse graphResponse) {
 
+                                LoginManager.getInstance().logOut();
+
+                            }
+                        }).executeAsync();
+
+                        back();
+                    }
+                });
+
+        alertDialog.setNegativeButton("Cancelar",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
     }
+
+    public void back(){
+        setContentView(R.layout.activity_login);
+    }
+
 
 
 }
